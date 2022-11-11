c2b55d80aadaa662dc83f6031d092fa49aef00e5
Curtis Byers <curtisbyers@pop-os.localdomain>
2022-11-11 14:59:26 -0800

    finally got it working

--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,26 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import collections
+
 
 class Scraper:
     def __init__(self, site):
         self.site = site
 
     def scrape(self):
+        newsroom = open("/home/curtisbyers/websites", 'w')
+        collections.Callable = collections.abc.Callable
         html = (urllib.request.urlopen(self.site)).read()
         parser = "html.parser"
         sp = BeautifulSoup(html, parser)
-        for tag in sp.findall("a"):
+        for tag in sp.find_all("a"):
             url = tag.get("href")
             if url is None:
                 continue
-            if "html" in url:
+            if "http" in url:
                 print("\n" + url)
+                newsroom.write("\n" + url)
 
-news = "https://news.google.com/home?hl=en-US&gl=US&ceid=US:en"
+
+news = "https://news.google.com/"
 Scraper(news).scrape()
